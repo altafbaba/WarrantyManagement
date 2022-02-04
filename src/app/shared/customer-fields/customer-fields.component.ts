b99660923cbadaefaea650c4aa1791b24cbd8339
Altaf <altafbaba143s@gmail.com>
2022-02-04 21:24:01 +0530

    customerfileds comp.

---
+++ src/app/shared/customer-fields/customer-fields.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-customer-fields',
+  templateUrl: './customer-fields.component.html',
+  styleUrls: ['./customer-fields.component.scss'],
+})
+export class CustomerFieldsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  customergroup: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    // lastName: new FormControl(''),
+    //date: new FormControl(''),
+    address: new FormControl(''),
+    email: new FormControl(''),
+    mobile: new FormControl(''),
+    state: new FormControl(''),
+    city: new FormControl(''),
+    zip: new FormControl(''),
+  });
+}
