e27f653ad4552eeeee3513f4345fa83f9db87e5a
Altaf <altafbaba143s@gmail.com>
2022-02-10 21:28:01 +0530

    Chk Gram

--- src/app/shared/customer-fields/customer-fields.component.ts
+++ src/app/shared/customer-fields/customer-fields.component.ts
@@ -11,13 +11,40 @@ import { map, startWith } from 'rxjs/operators';
 export class CustomerFieldsComponent implements OnInit {
   data: any[] = [];
   filteredOptions: Observable<any[]> | undefined;
+  //autofiled
+  displayFn(value: any) {
+    return value.name;
+  }
   constructor(private _getcustomer: CustomerService) {}
 
   ngOnInit(): void {
+    this.filteredOptions = this.customergroup.get('name')?.valueChanges.pipe(
+      startWith(''),
+      map((value) => {
+        return this._filter(value);
+      })
+    );
+
     //get Coustomer data form API
     this._getcustomer.getCustomers().subscribe((Response: any) => {
       this.data = Response.data;
     });
+  }
+  private _filter(value: any): any[] {
+    if (typeof value == 'object') {
+      //all input auto file
+      this.customergroup.patchValue(value);
+      const filterValue = value.name.toLowerCase();
+      return this.data.filter((option: any) =>
+        option.name.toLowerCase().includes(filterValue)
+      );
+    } else {
+      const filterValue = value.toLowerCase();
+
+      return this.data.filter((Option: any) =>
+        Option.name.toLowerCase().includes(filterValue)
+      );
+    }
   }
 
   customergroup: FormGroup = new FormGroup({
--- src/app/warranty/warranty-list/warranty-list.component.ts
+++ src/app/warranty/warranty-list/warranty-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DealerService } from 'src/app/core/dealer/dealer.service';
-
+import { AfterViewInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-warranty-list',
   templateUrl: './warranty-list.component.html',
@@ -8,8 +10,33 @@ import { DealerService } from 'src/app/core/dealer/dealer.service';
 })
 export class WarrantyListComponent implements OnInit {
   @Input() data: any[] = [];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  displayedColumns: string[] = [
+    'position',
+    'name',
+    'mobile',
+    'product',
+    'model',
+    'wsdate',
+    'wedate',
+  ];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
   constructor(private _dealerService: DealerService) {}
-
   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
+
+export interface PeriodicElement {
+  position: number;
+  name: string;
+  mobile: number;
+  product: string;
+  model: string;
+  wsdate: string;
+  wedate: string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [];
